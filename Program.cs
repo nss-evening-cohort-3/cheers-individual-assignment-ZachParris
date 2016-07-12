47fa9ecc6a24626b748b8dfb30217d583fea5cf2
ZachParris <zacharyjparris@gmail.com>
2016-07-11 21:20:41 -0500

    Added An for halfnorsemix

--- Program.cs
+++ Program.cs
@@ -12,14 +12,24 @@ namespace Cheers
             //Variables section
             String prompt = "> ";
             String cheer_prefix = "Give me a";
+            String requires_an = "aeiolnfshrmx";
 
             Console.WriteLine("What's your name?");
             Console.Write(prompt);
-            String user_name = Console.ReadLine(); //Read from terminal save to a variable
+            String user_name = Console.ReadLine().ToLower(); //Read from terminal save to a variable
 
             for(int i = 0; i < user_name.Length; i++) {
-                Console.WriteLine(cheer_prefix + ".." + user_name[i]);
+                if (requires_an.IndexOf(user_name[i]) > -1) {
+                    //Then its in the requires_an array!
+                    Console.WriteLine(cheer_prefix + "n" + ".." + user_name[i]);
+                }else{
+                    //Does not require "an"
+                    Console.WriteLine(cheer_prefix +  ".." + user_name[i]);
+                    
+                }
+                
             }
+
 
             //User_name.toUpper() is turning the name  to uppercase
             //ToUpper() accepts no arguments
